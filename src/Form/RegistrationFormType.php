da5303e98e89a3d76f0eeaed124552bbd0d7fdf0
OlraCode <cemcosta08@gmail.com>
2025-09-13 11:39:13 -0300

    add register validation

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -24,26 +26,33 @@ class RegistrationFormType extends AbstractType
                 'label' => 'Aceito os termos',
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Você deve aceitar os termos.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, options: [
+                'type' => PasswordType::class,
+                'first_options' =>
+                    [
+                        'label' => 'Senha',
+                        'attr' => ['autocomplete' => 'new-password'],
+                        'constraints' => [
+                            new NotBlank([
+                                'message' => 'Insira uma senha',
+                            ]),
+                            new Length([
+                                'min' => 6,
+                                'minMessage' => 'Senha deve conter no mínimo {{ limit }} caracteres',
+                                // max length allowed by Symfony for security reasons
+                                'max' => 20,
+                                'maxMessage' => 'Senha pode conter no máximo {{ limit }} caracteres',
+                            ]),
+                            new Regex('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)^', 'Senha deve conter pelo menos uma letra maiúscula, uma minuscula e um número.')]
+                    ],
+                'second_options' => ['label' => 'Confirmar Senha'],
+                'invalid_message' => 'As senhas não coincidem.',
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+
             ])
         ;
     }
