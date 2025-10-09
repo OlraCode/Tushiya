317a850d51192732ad65e7b828182a75f6de59fa
OlraCode <cemcosta08@gmail.com>
2025-10-09 08:32:24 -0300

    fix register error

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,10 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_course_index');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
