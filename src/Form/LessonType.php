e0a13b816ca1e7db0dd9ba48247cd21b5c2c232a
OlraCode <cemcosta08@gmail.com>
2025-11-12 18:09:08 -0300

    add create lesson function

---
+++ src/Form/LessonType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lesson;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class LessonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('video', UrlType::class)
+            ->add('title', options: [
+                'constraints' => [
+                    new Length(min: 4, max: 40)
+                ]
+            ])
+            ->add('button', SubmitType::class, ['label' => 'Cadastrar']);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+        ]);
+    }
+}
